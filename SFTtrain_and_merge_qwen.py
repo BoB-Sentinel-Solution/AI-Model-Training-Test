2e2cad92c0c6c16b249300fafddd079c38f2eb13
neck392 <neck3922@gmail.com>
2025-11-10 04:54:48 +0900

    Update SFTtrain_and_merge_qwen.py

--- SFTtrain_and_merge_qwen.py
+++ SFTtrain_and_merge_qwen.py
@@ -270,7 +270,7 @@ def main():
             load_in_4bit=True,
             bnb_4bit_compute_dtype=(torch.bfloat16 if args.bf16 else torch.float16),  # ← torch dtype(✅)
             bnb_4bit_quant_type="nf4",
-            bnb_4bit_use_double_quant=True,
+            bnb_4bit_use_double_quant=True
         )
 
     model = AutoModelForCausalLM.from_pretrained(
